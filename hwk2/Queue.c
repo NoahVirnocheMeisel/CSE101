b58f5641385c84898e78aaaddbcd3854ff193599
Noah Virnoche Meisel <nmeisel@unix3.lt.ucsc.edu>
2023-10-24 23:33:42 -0700

    trying to save

---
+++ hwk2/Queue.c
@@ -0,0 +1,96 @@
+#include "Queue.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+// Implemeting Queue
+
+/*
+
+It returns a pointer to the instantiated Queue
+*/
+Queue *initializeQueue(int n, char **array) {
+  Queue *Q = calloc(1, sizeof(Queue));
+  Q->size = n;
+  Q->array = array;
+  return Q;
+}
+//------------------------------------------------------------------------
+
+/*
+TODO: implement Queue destructor - this function frees memory allocated to
+Queue-make sure to delete strings and the array
+*/
+void destructQueue(Queue **q) {
+  if (q != NULL && *q != NULL) {
+    for (int i = 0; i < (*q)->size; i++) {
+      free((*q)->array[i]);
+    }
+    free((*q)->array);
+    free(*q);
+    *q = NULL;
+    free(q);
+    q = NULL;
+  }
+}
+//------------------------------------------------------------------------
+
+/* TODO: implement the 'enqueue' functionality
+This function takes in a queue pointer and a username and adds the username to
+queue
+*/
+// I hate this
+void enqueue(Queue *q, char *name) {
+  q->size++;
+  char **new = calloc(q->size, sizeof(char *));
+  if (q->array != NULL) {
+    for (int i = 0; i < q->size - 1; i++) {
+      new[i] = q->array[i];
+    }
+  }
+  new[q->size] = name;
+  free(q->array);
+  q->array = new;
+}
+
+//------------------------------------------------------------------------
+
+/* TODO: implement the 'dequeue' functionality
+This function takes in a queue pointer and removes element from head of the
+queue Print "Empty Queue" if there's not element to remove
+*/
+void dequeue(Queue *q) {
+  if (q->size == 0) {
+    printf("Empty Queue\n");
+  } else {
+    (q->size)--;
+    char **new = calloc(q->size, sizeof(char **));
+    for (int i = 0; i < q->size + 1; i++) {
+      new[i] = q->array[i + 1];
+    }
+    free(q->array);
+    q->array = new;
+  }
+}
+
+//------------------------------------------------------------------------
+
+/* TODO: implement the printQueue
+This function takes in a queue pointer and prints the contents of the queue in
+order (head to tail)
+*/
+
+char *front(Queue *q) { return q->array[0]; }
+void printQueue(Queue *q) {
+  for (int i = 0; i < q->size - 1; i++) {
+    printf("%s  ", q->array[i]);
+  }
+  printf("%s\n", q->array[q->size]);
+}
+
+//------------------------------------------------------------------------
+
+/* TODO: implement the count
+This function takes in a queue pointer and returns the 'count' field of the
+queue
+*/
+int count(Queue *q) { return q->size; }
---
+++ hwk2/simulator.c
@@ -0,0 +1,168 @@
+#include "List.h"
+#include "Queue.h"
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LEN 100 // code to create queue of array length = 100
+
+typedef struct Station {
+  char *dish_name;
+  int numberOfServings;
+  Queue *line;
+} Station;
+Station *add_station(char *dish, int num_serv, Queue *line);
+
+// frees the memory taken up by the station struct
+void close_station(Station **s);
+
+// returns the name of the station
+char *get_name(Station *s);
+
+// returns the number of servings at station
+int get_servings(Station *);
+
+// returns the queue pointer of the station
+Queue *get_line(Station *s);
+
+// function to add a number of servings(or potentially remove) to a Sation *s
+void restock_station(Station *s, int extra_servings);
+
+void printString(void *str) {
+  char *s = str;       // "casting" void * to char *
+  printf("%s    ", s); // print string starting from s
+}
+
+int main(int argc, char **argv) {
+  // creates a new station struct.
+
+
+
+  Station *access_station_by_name(ListPtr station_list, char *station_name,
+                                  int number_of_stations);
+  // code to create queue
+  char filename[50];
+  char station_name[50];
+  int num_dishes;
+  int station_num;
+  scanf("%d", &station_num);
+
+  ListPtr stations_list = createList(printString);
+
+  for (int i = 0; i < station_num; i++) {
+    scanf("%s %d", station_name, &num_dishes);
+    appendList(stations_list,
+               add_station(strdup(station_name), num_dishes, initializeQueue(0, NULL)));
+  }
+
+  ListPtr people_list = createList(printString);
+  scanf("%s", filename);
+  printf("%s\n",filename);
+  FILE *people_file = fopen(filename, "r");
+  char word[50];
+  int numberOfPeople;
+  fscanf(people_file,"%d",&numberOfPeople);
+  for (int i = 0; i < numberOfPeople;i++) {
+    fscanf(people_file,"%s %s",word,station_name);
+    appendList(people_list,add_station(strdup(word),num_dishes,NULL));
+  }
+
+
+  while (true) {
+    int command = -1;
+    bool first_command = true;
+    char *station_name;
+    int num_dishes;
+    char *person_name;
+
+    //checks if our command is good or not
+    do {
+      if (!first_command) {
+        printf("Invalid command, please type a number between 1 and 7");
+      }
+      first_command = false;
+      scanf("%d", &command);
+    } while (command < 1 || command > 7);
+
+    switch (command) {
+
+    case 1:
+      scanf("%s %s", person_name, station_name);
+      enqueue(get_line(access_station_by_name(stations_list, station_name, station_num)),person_name);
+      break;
+    case 2:
+      int dishes_to_add = 0;
+      scanf("% s % d", station_name, &dishes_to_add);
+      restock_station(
+          access_station_by_name(stations_list, station_name, station_num),
+          dishes_to_add);
+      break;
+    case 3:
+      scanf("% s % s", person_name, station_name);
+      
+
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      return 0;
+      break;
+    }
+  }
+
+  // code to read and process input
+
+  // code to free memory allocated to queue
+}
+
+
+//Helper functions and the Station struct. Normally would put in a separate file but not supposed to edit Makefile
+
+//find(LL *L,void *target,bool(*matcher)(void *, void*));
+//for each node:
+// if (matcher(target,cur->data)):
+// return cur->data
+
+//bool station_match(void * target,void *data):
+// char *name = (char *)target
+// Station* s = (station*)data
+//return strcmp(name, s->name)
+Station *access_station_by_name(ListPtr station_list, char *station_name,int number_of_stations) {
+  for (int i = 0; i < number_of_stations; i++) {
+    Station *current_station =getList(station_list,i);
+    if (strcmp(station_name, get_name(current_station)) == 0) {
+      return current_station;
+    }
+  }
+}
+Station *add_station(char *dish, int num_serv, Queue *line) {
+  Station *S = calloc(1, sizeof(Station));
+  S->dish_name = dish;
+  S->numberOfServings = num_serv;
+  S->line = line;
+  return S;
+}
+
+void close_station(Station **s) {
+  if (s != NULL && *s != NULL) {
+    destructQueue(&(*s)->line);
+    free(*s);
+    *s = NULL;
+    s = NULL;
+  }
+}
+
+char *get_name(Station *s) { return s->dish_name; }
+
+int get_servings(Station *s) { return s->numberOfServings; }
+
+Queue *get_line(Station *s) { return s->line; }
+
+void restock_station(Station *s, int extra_servings) {
+  s->numberOfServings = s->numberOfServings + extra_servings;
+}
